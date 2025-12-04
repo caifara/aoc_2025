b270afaa5f19428b6aee5e79c4115fa2a38cead6
Ivo Dancet <ivo.dancet@by2.be>
2025-12-04 08:49:19 +0100

    dag 4 p1

---
+++ day_4/parts.rb
@@ -0,0 +1,44 @@
+require_relative "../setup"
+
+class Part
+  def initialize(input)
+    @grid = input
+      .strip
+      .split("\n")
+      .map { |l| l.split("").map { |c| c == "@" } }
+  end
+end
+
+module Day4
+  class Part1 < Part
+    def solve
+      count = 0
+
+      @grid.each_with_index do |row, y|
+        row.each_with_index do |roll, x|
+          next unless roll
+
+          small_grid = begin
+              min_y = [y - 1, 0].max
+              max_y = y + 1
+              min_x = [x - 1, 0].max
+              max_x = x + 1
+
+              @grid[min_y..max_y].map { |r| r[min_x..max_x] }
+            end
+          count += 1 if small_grid.flatten.count(true) < 5
+        end
+      end
+
+      count
+    end
+  end
+
+  class Part2 < Part
+    def solve
+    end
+  end
+end
+
+puts Day4::Part1.from_input_file.solve
+# puts Day4::Part2.from_input_file.solve

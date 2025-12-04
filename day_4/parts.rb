be6028f17e264b976fffadf581d2026ffb686eb4
Ivo Dancet <ivo.dancet@by2.be>
2025-12-04 08:56:05 +0100

    low effort oplossing in de wachtzaal

--- day_4/parts.rb
+++ day_4/parts.rb
@@ -7,38 +7,57 @@ class Part
       .split("\n")
       .map { |l| l.split("").map { |c| c == "@" } }
   end
+
+  def removable_rolls(active_remove: false)
+    count = 0
+
+    @grid.each_with_index do |row, y|
+      row.each_with_index do |roll, x|
+        next unless roll
+
+        small_grid = begin
+            min_y = [y - 1, 0].max
+            max_y = y + 1
+            min_x = [x - 1, 0].max
+            max_x = x + 1
+
+            @grid[min_y..max_y].map { |r| r[min_x..max_x] }
+          end
+
+        if small_grid.flatten.count(true) < 5
+          @grid[y][x] = false if active_remove
+          count += 1
+        end
+      end
+    end
+
+    count
+  end
 end
 
 module Day4
   class Part1 < Part
     def solve
-      count = 0
-
-      @grid.each_with_index do |row, y|
-        row.each_with_index do |roll, x|
-          next unless roll
-
-          small_grid = begin
-              min_y = [y - 1, 0].max
-              max_y = y + 1
-              min_x = [x - 1, 0].max
-              max_x = x + 1
-
-              @grid[min_y..max_y].map { |r| r[min_x..max_x] }
-            end
-          count += 1 if small_grid.flatten.count(true) < 5
-        end
-      end
-
-      count
+      removable_rolls
     end
   end
 
   class Part2 < Part
     def solve
+      total = 0
+
+      loop do
+        removed_rolls_count = removable_rolls(active_remove: true)
+
+        break if removed_rolls_count.zero?
+
+        total += removed_rolls_count
+      end
+
+      total
     end
   end
 end
 
 puts Day4::Part1.from_input_file.solve
-# puts Day4::Part2.from_input_file.solve
+puts Day4::Part2.from_input_file.solve

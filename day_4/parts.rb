d8b78dec253605a903f0f1fe2527489f14232da4
Ivo Dancet <ivo.dancet@by2.be>
2025-12-04 18:28:53 +0100

    sneller en mooier

--- day_4/parts.rb
+++ day_4/parts.rb
@@ -1,4 +1,5 @@
 require_relative "../setup"
+require "benchmark"
 
 class Part
   def initialize(input)
@@ -8,30 +9,29 @@ class Part
       .map { |l| l.split("").map { |c| c == "@" } }
   end
 
-  def removable_rolls(active_remove: false)
-    count = 0
-
-    @grid.each_with_index do |row, y|
-      row.each_with_index do |roll, x|
+  def removable_rolls(remove_when_found: false)
+    @grid.each_with_index.sum do |row, y|
+      row.each_with_index.count do |roll, x|
         next unless roll
 
-        small_grid = begin
-            min_y = [y - 1, 0].max
-            max_y = y + 1
-            min_x = [x - 1, 0].max
-            max_x = x + 1
+        next if count_adjacent_rolls(x, y) > 3
 
-            @grid[min_y..max_y].map { |r| r[min_x..max_x] }
-          end
+        @grid[y][x] = false if remove_when_found
 
-        if small_grid.flatten.count(true) < 5
-          @grid[y][x] = false if active_remove
-          count += 1
-        end
+        true
       end
     end
+  end
 
-    count
+  private
+
+  def count_adjacent_rolls(x, y)
+    min_y = [y - 1, 0].max
+    max_y = y + 1
+    min_x = [x - 1, 0].max
+    max_x = x + 1
+
+    @grid[min_y..max_y].flat_map { |r| r[min_x..max_x] }.count(true) - 1
   end
 end
 
@@ -44,20 +44,13 @@ module Day4
 
   class Part2 < Part
     def solve
-      total = 0
-
-      loop do
-        removed_rolls_count = removable_rolls(active_remove: true)
-
-        break if removed_rolls_count.zero?
-
-        total += removed_rolls_count
-      end
-
-      total
+      Enumerator
+        .produce { removable_rolls(remove_when_found: true) }
+        .take_while(&:positive?)
+        .sum
     end
   end
 end
 
 puts Day4::Part1.from_input_file.solve
-puts Day4::Part2.from_input_file.solve
+puts Benchmark.measure { puts Day4::Part2.from_input_file.solve }.real

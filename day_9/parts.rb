7892d81a4039b697a3d3deee1145f9c1ceab3afe
Ivo Dancet <ivo.dancet@by2.be>
2025-12-10 22:27:25 +0100

    opkuis

--- day_9/parts.rb
+++ day_9/parts.rb
@@ -16,14 +16,14 @@ class Part2 < Part
   end
 
   def solve
-    $red_tile_shape = @red_tile_shape
     @red_tile_shape
       .points
       .combination(2)
       .map { |a, b| Rectangle.new(a, b) }
-      .select { |r| r.inside?(@red_tile_shape) }
-      .max_by(&:size)
-      .td
+      .sort_by(&:size)
+      .reverse
+      .find { |r| r.inside?(@red_tile_shape) }
+      .size
   end
 end
 
@@ -35,21 +35,22 @@ class Point
     @y = y
   end
 
-  # def line_segment_up
-  #   @line_segment_up ||= LineSegment.new(self, Point.new(x, 0))
-  # end
-  #
-  # def line_segment_down
-  #   @line_segment_down ||= LineSegment.new(self, Point.new(x, Float::INFINITY))
-  # end
-  #
-  # def line_segment_right
-  #   @line_segment_right ||= LineSegment.new(self, Point.new(Float::INFINITY, y))
-  # end
-  #
-  # def line_segment_left
-  #   @line_segment_left ||= LineSegment.new(self, Point.new(0, y))
-  # end
+  def ==(other)
+    @x == other.x && @y == other.y
+  end
+
+  def inside?(shape)
+    return true if shape.edges.any? { |e| e.contains?(self) }
+
+    second_point = Point.new(Float::INFINITY, @y)
+
+    ray = LineSegment.new(self, second_point)
+
+    intersection_count = shape.directional_edges(:vertical).count { |e| e.without_endpoints.intersect?(ray) } +
+                         shape.directional_edges(:horizontal).count { |e| ray.contains?(e) && shape.edge_is_entry?(e) }
+
+    intersection_count.odd?
+  end
 end
 
 class Shape
@@ -57,34 +58,36 @@ class Shape
 
   def initialize(points)
     @points = points
-    @internal_lines = {
-      horizontal: {},
-      vertical: {}
-    }
   end
 
   def edges
-    @edges ||= @points.combination(2).map { |a, b| LineSegment.new(a, b) }
+    @edges ||= (@points + [@points.first]).each_cons(2).map { |a, b| LineSegment.new(a, b) }
   end
 
-  def vertical_edges
-    @vertical_edges ||= edges.select { |e| e.point1.x == e.point2.x }
+  def directional_edges(direction)
+    @directional_edges ||= {}
+    @directional_edges[direction] ||= edges.select { |e| e.direction == direction }
   end
 
-  def horizontal_edges
-    @horizontal_edges ||= edges.select { |e| e.point1.y == e.point2.y }
-  end
+  #                                                           |‾
+  # edges are entries if they define a staircase like form _|‾
+  # if a ray is on that edge, it will enter or exit the shape
+  #
+  # if the shape is like a tower _|‾|_ the ray moving along the top will just
+  # touch the shape (or enter + exit)
+  #
+  # as only used for horizontal rays, i didn't care about making this work for vertical edges
+  def edge_is_entry?(edge)
+    raise unless edge.horizontal?
+    raise unless edges.include?(edge)
 
-  def includes_line?(line)
-    internal_lines(line.direction)
-  end
+    edge_1 = directional_edges(edge.other_direction).find { |e| e.points.include?(edge.point1) }
+    edge_2 = directional_edges(edge.other_direction).find { |e| e.points.include?(edge.point2) }
 
-  def internal_lines(line.direction)
-    if line.horizontal?
-      vertical_edges.collect { |e| e.intersection_point }
-    else
-      horizontal_edges.collect { |e| e.intersection_point }
-    end
+    edge_1_other_y = (edge_1.points - [edge.point1]).first.y
+    edge_2_other_y = (edge_2.points - [edge.point2]).first.y
+
+    ((edge_1_other_y - edge.y) * (edge_2_other_y - edge.y)).negative?
   end
 end
 
@@ -100,22 +103,7 @@ class Rectangle < Shape
   end
 
   def inside?(shape)
-    # return false unless points.all? do |p|
-    #   shape.points.include?(p) ||
-    #     (shape.horizontal_edges.any? { |se| se.intersect?(p.line_segment_left) } ||
-    #      shape.horizontal_edges.any? { |se| se.intersect?(p.line_segment_right) }) ||
-    #     (shape.vertical_edges.any? { |se| se.intersect?(p.line_segment_up) } ||
-    #      shape.vertical_edges.any? { |se| se.intersect?(p.line_segment_down) })
-    # end
-    #
-    # return false if vertical_edges.any? do |e|
-    #   shape.horizontal_edges.any? { |se| se.intersect?(e) }
-    # end
-    #
-    # horizontal_edges.none? do |e|
-    #   shape.vertical_edges.any? { |se| se.intersect?(e) }
-    # end
-    edges.all? { |edge| shape.includes_line?(edge) }
+    @points.all? { |p| p.inside?(shape) } && edges.none? { |e| e.without_endpoints.intersect?(shape) }
   end
 end
 
@@ -127,12 +115,74 @@ class LineSegment
     @point2 = point2
   end
 
+  def points = [@point1, @point2]
+
+  def x
+    raise unless vertical?
+
+    @point1.x
+  end
+
+  def y
+    raise unless horizontal?
+
+    @point1.y
+  end
+
+  def without_endpoints
+    if horizontal?
+      x1, x2 = [point1.x, point2.x].sort.then { |x1, x2| [x1 + 1, x2 - 1] }
+      LineSegment.new(Point.new(x1, y), Point.new(x2, y))
+    else
+      y1, y2 = [point1.y, point2.y].sort.then { |y1, y2| [y1 + 1, y2 - 1] }
+      LineSegment.new(Point.new(x, y1), Point.new(x, y2))
+    end
+  end
+
   def intersect?(other)
-    (point1.x..point2.x).cover?(other.point1.x) && (point1.y..point2.y).cover?(other.point1.y)
+    case other
+    when Shape
+      other.directional_edges(other_direction).any? { |e| e.without_endpoints.intersect?(self) }
+    when LineSegment
+      raise if direction == other.direction
+
+      return false unless direction == :horizontal ? x_range.cover?(other.x) : y_range.cover?(other.y)
+
+      direction == :horizontal ? other.y_range.cover?(y) : other.x_range.cover?(x)
+    else raise
+    end
+  end
+
+  def direction = point1.x == point2.x ? :vertical : :horizontal
+  def other_direction = direction == :vertical ? :horizontal : :vertical
+  def vertical? = direction == :vertical
+  def horizontal? = direction == :horizontal
+
+  def intersection_point(other)
+    return nil unless intersect?(other)
+
+    if direction == :horizontal
+      Point.new(other.x, y)
+    else
+      Point.new(x, other.y)
+    end
+  end
+
+  def x_range = @x_range ||= Range.new(*[point1.x, point2.x].sort)
+  def y_range = @y_range ||= Range.new(*[point1.y, point2.y].sort)
+
+  def contains?(other)
+    case other
+    when LineSegment
+      x_range.cover?(other.x_range) && y_range.cover?(other.y_range)
+    when Point
+      x_range.cover?(other.x) && y_range.cover?(other.y)
+    else raise
+    end
   end
 end
 
 puzzle = Puzzle.new(file: __FILE__, test: ARGV.include?("test"))
 
 puts(Benchmark.ms { puts Part1.new(puzzle).solve })
-puts(Benchmark.ms { puts Part2.new(puzzle).solve })
+puts(Benchmark.ms { pp Part2.new(puzzle).solve })
